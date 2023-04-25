d8814377d49c6a44e4092770f85309a818293b1e
oumar969 <114076085+oumar969@users.noreply.github.com>
2023-04-25 20:06:05 +0200

    UnitTest-Example

---
+++ Unit Test/Clock/Test/ClockTest.java
@@ -0,0 +1,91 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertThrows;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class ClockTest {
+
+    private void checkClock(Clock clock, int hour, int minute, int second) {
+        assertEquals(hour, clock.getHour());
+        assertEquals(minute, clock.getMinute());
+        assertEquals(second, clock.getSecond());
+    }
+
+    @Test
+    public void lowConstructorParameters() {
+        Clock clock = new Clock(0, 0, 0);
+        checkClock(clock, 0, 0, 0);
+    }
+
+    @Test
+    public void highConstructorParameters() {
+        Clock clock = new Clock(23, 59, 59);
+        checkClock(clock, 23, 59, 59);
+    }
+
+    @Test
+    public void middleConstructorParameters() {
+        //error detected
+        Clock clock = new Clock(11, 27, 41);
+        checkClock(clock, 11, 27, 41);
+    }
+
+    @Test()
+    public void illeargumentExceptionOnHour() {
+        //error detected
+        assertThrows(IllegalArgumentException.class, () -> new Clock(24, 0, 0));
+    }
+
+    @Test()
+    public void illeargumentExceptionOnMinute() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock(0, 60, 0));
+    }
+
+    @Test()
+    public void illeargumentExceptionOnSecond() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock(0, 0, 60));
+    }
+
+    @Test()
+    public void setSecondsTurnsToMinutes() {
+        Clock clock = new Clock(65);
+        //error detected, see also middleConstructorParameters
+        checkClock(clock, 0, 1, 5);
+    }
+
+    @Test()
+    public void setSecondsTurnsMinutesToHours() {
+        Clock clock = new Clock(2 * 3600 + 10 * 60 + 15);
+        checkClock(clock, 2, 10, 15);
+        //error detected, see also middleConstructorParameters
+    }
+
+    @Test()
+    public void ticUpdatesSecond() {
+        Clock clock = new Clock(13, 12, 58);
+        clock.tic();
+        checkClock(clock, 13, 12, 59);
+    }
+
+    @Test()
+    public void ticUpdatesMinute() {
+        Clock clock = new Clock(13, 12, 59);
+        clock.tic();
+        checkClock(clock, 13, 13, 0);
+    }
+
+    @Test()
+    public void ticUpdatesHour() {
+        Clock clock = new Clock(13, 59, 59);
+        clock.tic();
+        checkClock(clock, 14, 0, 0);
+    }
+
+    @Test()
+    public void ticChangesDay() {
+        Clock clock = new Clock(23, 59, 59);
+        clock.tic();
+        //error detected
+        checkClock(clock, 0, 0, 0);
+    }
+}
---
+++ Unit Test/Queues/Test/QueueTest.java
@@ -0,0 +1,183 @@
+import org.junit.jupiter.api.Test;
+import queue.QueueADT;
+import queueimpls.SimpleBoundedArrayQueueC;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.testng.Assert.assertThrows;
+import static org.testng.AssertJUnit.*;
+
+public class QueueTest {
+
+    private QueueADT<String> getQueue(int capacity) {
+        //return new SimpleBoundedArrayQueueCorrect<>(capacity);
+        return new SimpleBoundedArrayQueueC<>(capacity);
+    }
+
+    QueueADT<String> queue;
+
+    @Test
+    public void testConstructor() {
+        queue = getQueue(0);
+        queue = getQueue(1);
+        queue = getQueue(5);
+    }
+
+    @Test
+    public void testConstructorNegativeCap() {
+        getQueue(-1);
+    }
+
+    @Test
+    public void enqueueString() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+    }
+
+    @Test
+    public void enqueueNull() {
+        queue = getQueue(5);
+        assertThrows(IllegalArgumentException.class, () -> queue.enqueue(null));
+    }
+
+    @Test
+    public void enqueuePastCapacity() {
+        queue = getQueue(5);
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue("a");
+        }
+        assertThrows(IllegalStateException.class, () -> queue.enqueue("a"));
+    }
+
+    @Test
+    public void dequeueFromEmptyQueue() {
+        queue = getQueue(5);
+        assertThrows(IllegalStateException.class, () -> queue.dequeue());
+        queue.enqueue("a");
+        assertDoesNotThrow(() -> queue.dequeue());
+        assertThrows(IllegalStateException.class, () -> queue.dequeue());
+    }
+
+    @Test
+    public void dequeueCorrectSequence() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+        queue.enqueue("to");
+        queue.enqueue("tre");
+        assertEquals("en", queue.dequeue());
+        assertEquals("to", queue.dequeue());
+        queue.enqueue("fire");
+        assertEquals("tre", queue.dequeue());
+        assertEquals("fire", queue.dequeue());
+    }
+
+    @Test
+    public void firstOnEmptyQueue() {
+        queue = getQueue(5);
+        assertThrows(IllegalStateException.class, () -> queue.first());
+        queue.enqueue("a");
+        assertDoesNotThrow(() -> queue.first());
+        queue.dequeue();
+        assertThrows(IllegalStateException.class, () -> queue.first());
+    }
+
+    @Test
+    public void testFirst() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+        queue.enqueue("to");
+        queue.enqueue("tre");
+        assertEquals("en", queue.first());
+        assertEquals("en", queue.first());
+        assertEquals("en", queue.dequeue());
+        assertEquals("to", queue.first());
+        assertEquals("to", queue.dequeue());
+        assertEquals("tre", queue.first());
+        assertEquals("tre", queue.dequeue());
+    }
+
+    @Test
+    public void testSizeEmptyQueue() {
+        queue = getQueue(5);
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+
+    public void testSizeNonEmptyQueue() {
+        queue = getQueue(5);
+        assertEquals(0, queue.size());
+        queue.enqueue("en");
+        assertEquals(1, queue.size());
+        queue.enqueue("to");
+        assertEquals(2, queue.size());
+        queue.enqueue("tre");
+        assertEquals(3, queue.size());
+    }
+
+    @Test
+    public void testisEmpty() {
+        queue = getQueue(5);
+        assertTrue(queue.isEmpty());
+        queue.enqueue("en");
+        assertFalse(queue.isEmpty());
+        queue.dequeue();
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testIndexEfterEnqueue() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+        queue.enqueue("to");
+        queue.enqueue("tre");
+        queue.enqueue("en");
+        assertEquals(0, queue.indexOf("en"));
+        assertEquals(1, queue.indexOf("to"));
+        assertEquals(2, queue.indexOf("tre"));
+        queue.dequeue();
+        assertEquals(0, queue.indexOf("to"));
+        assertEquals(1, queue.indexOf("tre"));
+        assertEquals(2, queue.indexOf("en"));
+        queue.dequeue();
+        assertEquals(-1, queue.indexOf("to"));
+        assertEquals(0, queue.indexOf("tre"));
+        assertEquals(1, queue.indexOf("en"));
+    }
+
+    @Test
+    public void testContainsEmptyQueue() {
+        //error in SimpleBoundedArrayQueueCorrect found
+        queue = getQueue(5);
+        assertFalse(queue.contains("en"));
+    }
+    @Test
+    public void testContainsAfterEnqueue() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+        queue.enqueue("to");
+        queue.enqueue("tre");
+        assertTrue(queue.contains("en"));
+        assertTrue(queue.contains("to"));
+        assertTrue(queue.contains("tre"));
+        assertFalse(queue.contains("fire"));
+        queue.dequeue();
+        assertFalse(queue.contains("en"));
+    }
+
+    @Test
+    public void testToStringEmptyQueue() {
+        queue = getQueue(5);
+        assertEquals("{}", queue.toString());
+    }
+
+    @Test
+    public void testToStringAfterEnqueue() {
+        queue = getQueue(5);
+        queue.enqueue("en");
+        queue.enqueue("to");
+        queue.enqueue("tre");
+        queue.enqueue("fire");
+        queue.enqueue("fem");
+        assertEquals("{en, to, tre, fire, fem}", queue.toString());
+    }
+}
