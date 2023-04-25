ed98f3ffc08d91028ef1981ad3d03a8afe6a7319
oumar969 <114076085+oumar969@users.noreply.github.com>
2023-04-25 20:46:04 +0200

    UnitTest-Example

---
+++ UnitTest1/LoginApplicationTest/src/test/TestLogin.java
@@ -0,0 +1,48 @@
+package test;
+
+import javafx.beans.property.SimpleStringProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.com.example.loginapplicationtest.core.ModelFactory;
+import java.com.example.loginapplicationtest.core.ViewModelFactory;
+import java.com.example.loginapplicationtest.viewmodels.LoginVM;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestLogin
+{
+
+  private ViewModelFactory vmf;
+
+  @BeforeEach
+  public void setup()
+  {
+    ModelFactory mf = new ModelFactory();
+    vmf = new ViewModelFactory(mf);
+  }
+
+  @Test
+  public void testStuff()
+  {
+    //Arrange
+    LoginVM loginVM = vmf.getLoginVM();
+
+    SimpleStringProperty username = new SimpleStringProperty();
+    SimpleStringProperty password = new SimpleStringProperty();
+    SimpleStringProperty resultLabel = new SimpleStringProperty();
+
+    username.bindBidirectional(loginVM.userNameProperty());
+    password.bindBidirectional(loginVM.passwordProperty());
+    resultLabel.bindBidirectional(loginVM.loginResultProperty());
+
+    //act
+    username.setValue("Michael");
+    password.setValue("Michael1");
+    loginVM.validateLogin();
+
+    //Assert
+    assertEquals("OK", resultLabel.getValue());
+  }
+
+}
